aa67f3b291b25af962811c2b42e04b4490a3d311
Matías Cassina <maticassina04@gmail.com>
2025-10-18 15:49:57 -0300

    agrego pagina propiedades-destacadas

--- alojaApp/Front/src/App.jsx
+++ alojaApp/Front/src/App.jsx
@@ -4,6 +4,7 @@ import Login from "./pages/login.jsx";
 import Profile from "./pages/Profile.jsx";
 import ProtectedRoute from "./components/ProtectedRoute.jsx";
 import Search from "./pages/search.jsx";  // importa el componente Search (export default)
+import PropiedadesEncontradas from "./pages/propiedades-encontradas.jsx";
 
 export default function App() {
   return (
@@ -11,6 +12,7 @@ export default function App() {
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
       <Route path="/buscar" element={<Search />} />   {/* <-- FIX */}
+      <Route path="/propiedades-encontradas" element={<PropiedadesEncontradas />} />
       <Route
         path="/profile"
         element={
