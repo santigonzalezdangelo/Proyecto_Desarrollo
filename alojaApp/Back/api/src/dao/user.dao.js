e85436122940efa5c43b059457ed867ed7f0cfef
Matias <matiochoa3004@gmail.com>
2025-10-20 20:21:45 -0300

    fix login

--- alojaApp/Back/api/src/dao/user.dao.js
+++ alojaApp/Back/api/src/dao/user.dao.js
@@ -1,18 +1,21 @@
-
+// src/dao/user.dao.js
 import PostgresDAO from "./postgres.dao.js";
-import {userModel, roleModel} from "../models/associations.js";
+import { userModel, roleModel } from "../models/associations.js";
 
 class UserDAO extends PostgresDAO {
   constructor() {
     super(userModel);
-  } 
+  }
 
   findByEmail = async (email) => {
     try {
-      return await this.model.findOne({ where: { correo: email }, include: roleModel });
+      return await this.model.findOne({
+        where: { correo: email },
+        include: [{ model: roleModel, as: "rol", required: false }],
+      });
     } catch (error) {
       console.error("Error fetching user by email:", error);
-      throw new Error(error);
+      throw error;
     }
   };
 
@@ -21,24 +24,21 @@ class UserDAO extends PostgresDAO {
       return await this.model.create(data);
     } catch (error) {
       console.error("Error creating user:", error);
-      throw new Error(error);
-    } 
+      throw error;
+    }
   };
 
   findById = async (id_usuario) => {
     try {
-      const user = await this.model.findOne({
+      return await this.model.findOne({
         where: { id_usuario },
-        include: [
-          { model: roleModel, as: "Role", required: false } // para user.Role?.nombre_rol
-        ],
+        include: [{ model: roleModel, as: "rol", required: false }],
       });
-      return user; // puede ser null si no existe
     } catch (error) {
       console.error("Error in UserDAO.findById:", error);
       throw error;
     }
   };
 }
-export default new UserDAO();
 
+export default new UserDAO();
